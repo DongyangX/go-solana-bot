f2fb9d0e17fa31caa421cfaff56a7258a958a8c8
DongyangX <pengdy9527@gmail.com>
2025-10-26 11:25:21 +0800

    Get price query up to 50 ids at once

--- subPrice/main.go
+++ subPrice/main.go
@@ -33,6 +33,7 @@ func main() {
 		// 1 Update Current Price
 		err := UpdateCurrentPrice(db, config)
 		if err != nil {
+			fmt.Println(err)
 			continue
 		}
 
@@ -106,23 +107,42 @@ func GetPrice(positions []common.Position, config *utils.Config) (map[string]map
 		ids = append(ids, position.Token)
 	}
 
-	// You can query up to 50 ids at once.
-	if len(ids) > 50 {
-		ids = ids[:50]
-	}
-	idsStr := strings.Join(ids, ",")
-
 	// Get Price
-	pUrl := config.PriceUrl + "?ids=" + idsStr
-	resp, err := utils.HttpProxyGet(pUrl)
-	fmt.Println("GetPrice Response:", string(resp))
+	pUrl := config.PriceUrl + "?ids="
 
+	// You can query up to 50 ids at once.
 	priceMap := make(map[string]map[string]float64)
-	err = json.Unmarshal(resp, &priceMap)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
+
+	count := len(ids) / 50
+	remainder := len(ids) % 50
+	for i := 0; i <= count; i++ {
+		var idsStr string
+		if count == i {
+			if remainder > 0 {
+				idsStr = strings.Join(ids[i*50:], ",")
+				fmt.Println(ids[i*50:])
+			} else {
+				break
+			}
+		} else {
+			idsStr = strings.Join(ids[i*50:(i+1)*50], ",")
+		}
+		pUrl = pUrl + idsStr
+		resp, err := utils.HttpProxyGet(pUrl)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("GetPrice Response:", string(resp))
+		tempMap := make(map[string]map[string]float64)
+		err = json.Unmarshal(resp, &tempMap)
+		if err != nil {
+			return nil, err
+		}
+		for k, v := range tempMap {
+			priceMap[k] = v
+		}
 	}
+
 	return priceMap, nil
 }
 
